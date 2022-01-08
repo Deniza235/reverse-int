008fa407a585a423e5d529e179ca0a8ce87c4d1e
github-deniza235 <lobashova.mariya@mail.ru>
2022-01-08 13:30:54 +0300

    feat: add solution task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    if (n >  0) {
+        let newN = n.toString().split('').reverse().join('');
+        return newN;
+    } else {
+        n = Math.abs(n);
+        let newN = n.toString().split('').reverse().join('');
+        return newN;
+    }
 }
